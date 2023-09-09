d961bc2e870746c010f99e7f6ff8d42f7429522a
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-09-10 01:45:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goostrack_front">
+      <BrowserRouter basename="/react_node_fs71_Nepalcem">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
